25c591e2afea4443c84330eeeb79c04fb7011c7e
AkaDeipu <santoshr3dd4@gmail.com>
2025-11-06 16:44:42 +0530

    Fix typo in confidence scores header

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if st.button("Analyze Sentiment"):
         sentiment_map = {0: "Negative", 1: "Neutral", 2: "Positive"}
         #st.write(f"Predicted Sentiment: {sentiment_map.get(prediction, 'Unknown')}")
         st.markdown(f"## Predicted Sentiment: {sentiment_emojis.get(prediction, 'Unknown')}")
-        st.write(f"##Confidence Scores:")
+        st.write(f"## Confidence Scores:")
         st.write({
             "Negative": round(proba[0], 3),
             "Neutral": round(proba[1], 3),
