d69796303e786c627d4341b51c8135c43171b599
AkaDeipu <santoshr3dd4@gmail.com>
2025-11-06 16:40:22 +0530

    Refactor sentiment display in app.py
    Updated sentiment display format and added markdown for predicted sentiment.

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ if st.button("Analyze Sentiment"):
         proba = pipeline.predict_proba([review])[0]
         prediction = proba.argmax()
         sentiment_map = {0: "Negative", 1: "Neutral", 2: "Positive"}
-        st.write(f"Predicted Sentiment: {sentiment_map.get(prediction, 'Unknown')}")
-        #st.markdown(f"## Predicted Sentiment: {sentiment_emojis.get(prediction, 'Unknown')}")
-        st.write(f"Confidence Scores:")
-        st.write({
-            "Negative": round(proba[0], 3),
-            "Neutral": round(proba[1], 3),
-            "Positive": round(proba[2], 3)})
+        #st.write(f"Predicted Sentiment: {sentiment_map.get(prediction, 'Unknown')}")
+        st.markdown(f"## Predicted Sentiment: {sentiment_emojis.get(prediction, 'Unknown')}")
+        st.write(f"##Confidence Scores:")
+        st.write(f"{##
+            "Negative": round(proba[0], 3)}
+            {"Neutral": round(proba[1], 3)},
+            {"Positive": round(proba[2], 3)}")
     else:
         st.warning("Please enter a review to analyze.")
